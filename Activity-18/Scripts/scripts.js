a6df62e4aad02625c5fc75cd9054fcb1873ac423
Rajaten10 <rajababu@go.byuh.edu>
2024-06-18 21:53:02 -1000

    done

--- Activity-18/Scripts/scripts.js
+++ Activity-18/Scripts/scripts.js
@@ -2,12 +2,12 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     event.preventDefault(); // Prevent form from submitting the traditional way
 
     var username = document.getElementById('username').value;
-    var phoneNumber =document.getElementById('phoneNumber').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
     var usernameValid = /^[a-zA-Z0-9]{5,}$/.test(username); // Username should be at least 5 characters long and contain only letters and numbers
-    var phoneNumberValid = /^\+?[1-9]\d{1,14}$/.test(phoneNumber);
+    var phoneNumberValid = /^\+?(\d{1,3})?[-.\s]?\(?\d{1,4}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(phoneNumber);
     var emailValid = /^[^@]+@\w+(\.\w+)+\w$/.test(email); // Simple email pattern check
     var passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password); // Password should be at least 8 characters long, contain numbers and both lowercase and uppercase letters
 
@@ -26,7 +26,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     if (formValid) {
         document.getElementById('registrationFeedback').textContent = 'Your user registration was accepted!';
         document.getElementById('registrationFeedback').style.display = 'block';
-        // Here you can also handle the form submission, e.g. send data to the server
+        
     } else {
         document.getElementById('registrationFeedback').textContent = '';
         document.getElementById('registrationFeedback').style.display = 'none';
